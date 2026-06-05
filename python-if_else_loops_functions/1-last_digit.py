9114f9d0862e5058fdef4e1e8f5ba741f36ea2b1
wahabxvii <auwa.mm@gmail.com>
2026-06-05 22:50:13 +0300

    fix: printing negative numbers

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -2,7 +2,10 @@
 import random
 
 number = random.randint(-10000, 10000)
-last_digit = abs(number) % 10
+if number < 0:
+    last_digit = number % 10 - 10
+else:
+    last_digit = abs(number) % 10
 if last_digit == 0:
     print(f"Last digit of {number} is {last_digit} and is 0")
 elif last_digit < 6:
