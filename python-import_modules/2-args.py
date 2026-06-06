1113272489e5e1ea652df2ffb381ec5a90221fa5
wahabxvii <auwa.mm@gmail.com>
2026-06-06 22:51:49 +0300

    create: file 2

---
+++ python-import_modules/2-args.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+import sys
+
+
+def print_args():
+    args = len(sys.argv)
+    if args < 2:
+        print("{0} arguments.".format(args - 1))
+    else:
+        print("{0} arguments:".format(args - 1))
+    for i in range(1, args):
+        print("{0}: {1}".format(i, sys.argv[i]))
+
+
+if __name__ == "__main__":
+    print_args()
