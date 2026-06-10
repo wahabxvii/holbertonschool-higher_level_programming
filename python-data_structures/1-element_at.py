808fa0d48f3ca6cda369c68f22d8cc2b98d69aa4
wahabxvii <auwa.mm@gmail.com>
2026-06-10 15:06:58 +0300

    fix

--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -4,7 +4,7 @@
 def element_at(my_list, idx):
     if idx < 0:
         print("Element at index {0:d} is None".format(idx))
-    elif idx > len(my_list):
+    elif idx >= len(my_list):
         print("Element at index {0:d} is None".format(idx))
     else:
         print("Element at index {0:d} is {1:d}".format(idx, my_list[idx]))
