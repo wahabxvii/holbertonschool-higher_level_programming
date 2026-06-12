190ab249b763bde057e1de635c9ec580ce0b4fa6
wahabxvii <auwa.mm@gmail.com>
2026-06-12 21:59:34 +0300

    fix: pycodestyle

--- python-data_structures/10-divisible_by_2.py
+++ python-data_structures/10-divisible_by_2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
-    t_f_list=[]
+    t_f_list = []
     for int in range(len(my_list)):
         if my_list[int] % 2 == 0:
             t_f_list.append(True)
