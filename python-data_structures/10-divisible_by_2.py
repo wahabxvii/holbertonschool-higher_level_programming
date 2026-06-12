01d09e4d1f3a05b6de276c627e83d66cf5465dc7
wahabxvii <auwa.mm@gmail.com>
2026-06-12 21:57:39 +0300

    fix: use range len for list length

--- python-data_structures/10-divisible_by_2.py
+++ python-data_structures/10-divisible_by_2.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
     t_f_list=[]
-    for int in my_list:
+    for int in range(len(my_list)):
         if my_list[int] % 2 == 0:
             t_f_list.append(True)
         else:
