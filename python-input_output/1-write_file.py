ec7fde790178ce46bec76bc53f7298c68bcb5481
wahabxvii <auwa.mm@gmail.com>
2026-07-04 23:57:18 +0300

    fix

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -5,4 +5,4 @@
 def write_file(filename="", text=""):
     """Function that writes a string to a text file."""
     with open(filename, 'w', encoding="utf-8") as f:
-        f.write(text)
+        return f.write(text)
