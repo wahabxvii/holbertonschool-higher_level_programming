33709e5df436daf96b399f8810fa7e11c39be6a6
wahabxvii <auwa.mm@gmail.com>
2026-07-24 17:06:31 +0300

    fix: typo

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -1,5 +1,5 @@
 -- script that creates a full table
-CREATE TABLE IF NOT EXIST second_table (
+CREATE TABLE IF NOT EXISTS second_table (
 	id INT, name VARCHAR(256), score INT
 );
 INSERT INTO second_table (
