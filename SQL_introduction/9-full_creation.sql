90d373017f290b78fcee75bef9a2fd277dd8bf5b
wahabxvii <auwa.mm@gmail.com>
2026-07-24 17:14:24 +0300

    fix: remove column names from values

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -4,9 +4,8 @@ CREATE TABLE IF NOT EXISTS second_table (
 );
 INSERT INTO second_table (
 	id, name, score
-) VALUES (
-	(id = 1, name = "John", score = 10),
-	(id = 2, name = "Alex", score = 3),
-	(id = 3, name = "Bop", score = 14),
-	(id = 4, name = "George", score = 8)
-);
+) VALUES
+	(1, "John", 10),
+	(2, "Alex", 3),
+	(3, "Bop", 14),
+	(4, "George", 8);
