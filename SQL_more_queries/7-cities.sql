5e736970ea7cbeb080ad6eeaf36799efe69f767f
wahabxvii <auwa.mm@gmail.com>
2026-07-26 13:22:55 +0300

    fix: missing commas

--- SQL_more_queries/7-cities.sql
+++ SQL_more_queries/7-cities.sql
@@ -4,7 +4,7 @@ USE hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS cities (
 	id INT AUTO_INCREMENT PRIMARY KEY,
 	state_id INT NOT NULL,
-	name VARCHAR(256) NOT NULL
+	name VARCHAR(256) NOT NULL,
 	CONSTRAINT fk_cities_states FOREIGN KEY (state_id)
-	REFERENCES states(id),
+	REFERENCES states(id)
 );
