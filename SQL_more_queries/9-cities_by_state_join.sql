76d48d135c28b47aa0ea38ed207fca667f74cdc1
wahabxvii <auwa.mm@gmail.com>
2026-07-26 16:17:15 +0300

    fix: typo

--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -4,5 +4,5 @@ SELECT
 	cities.name AS city_name,
 	states.name AS state_name
 	FROM cities
-	INNER JOIN states ON cities.state_id = sstates.id
+	INNER JOIN states ON cities.state_id = states.id
 	ORDER BY cities.id ASC;
