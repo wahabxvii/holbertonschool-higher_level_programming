0e32f80fa4a6d9f0743088151a57cd25e9ed0dce
wahabxvii <auwa.mm@gmail.com>
2026-07-27 01:03:47 +0300

    f

--- SQL_more_queries/12-no_genre.sql
+++ SQL_more_queries/12-no_genre.sql
@@ -1,10 +1,9 @@
--- script that lists all shows in hbtn_0d_tvshows without genre
+-- script that lists all genres in hbtn_0d_tvshows
+-- and displays number of shows
 SELECT
-	tv_shows.title,
-	tv_show_genres.genre_id
-	FROM tv_shows
-	LEFT JOIN tv_show_genres ON tv_shows.id =
-				tv_show_genres.show_id
-	WHERE tv_show_genres.genre_id IS NULL
-	ORDER BY tv_shows.title ASC,
-		tv_show_genres.genre_id ASC;
+	tv_genres.name AS genre,
+	COUNT(*) AS number_of_shows
+	FROM tv_genres
+	INNER JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
+	GROUP BY tv_genres.name
+	ORDER BY number_of_shows DESC;
