6c626156a541b22ffc9abc8719ffe21849356c32
wahabxvii <auwa.mm@gmail.com>
2026-07-27 01:35:36 +0300

    create: file 14

---
+++ SQL_more_queries/14-my_genres.sql
@@ -0,0 +1,6 @@
+-- script that uses the detabase to list all genres of a show
+SELECT
+	tv_genres.name
+	FROM tv_genres
+	INNER JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
+	WHERE tv_show_genres.show_id = 8
