e44a41915d01bda38fc54788b514e687535e568f
wahabxvii <auwa.mm@gmail.com>
2026-07-26 16:19:02 +0300

    fix: remove names

--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -1,8 +1,8 @@
 -- script that lists all cities
 SELECT
-	cities.id AS city_id,
-	cities.name AS city_name,
-	states.name AS state_name
+	cities.id,
+	cities.name,
+	states.name
 	FROM cities
 	INNER JOIN states ON cities.state_id = states.id
 	ORDER BY cities.id ASC;
