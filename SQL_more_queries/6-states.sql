189270e7779fb00310d3c68adbbfd10973546af0
wahabxvii <auwa.mm@gmail.com>
2026-07-26 11:54:56 +0300

    create: file 6

---
+++ SQL_more_queries/6-states.sql
@@ -0,0 +1,6 @@
+-- script that creates the database hbtn_0d-usa and table states
+CREATE DATABASE IF NOT EXISTS hbtn_0d-usa;
+CREATE TABLE IF NOT EXISTS states (
+	id INT UNIQUE AUTO_INCREMENT NOT NULL PRIMARY KEY,
+	name VARCHAR(256) NOT NULL
+);
