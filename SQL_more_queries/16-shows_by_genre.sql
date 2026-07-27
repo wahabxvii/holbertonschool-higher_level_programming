f4db894ee95e6cd190bd58c04b69eadbb025d420
wahabxvii <auwa.mm@gmail.com>
2026-07-27 03:42:37 +0300

    fix: name the field

--- SQL_more_queries/16-shows_by_genre.sql
+++ SQL_more_queries/16-shows_by_genre.sql
@@ -1,7 +1,7 @@
 -- script that lists all shows and all genres linked to that show
 SELECT
 	tv_shows.title,
-	NULLIF(tv_genres.name, '')
+	NULLIF(tv_genres.name, '') AS name
 	FROM tv_shows
 	LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
 	LEFT JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
