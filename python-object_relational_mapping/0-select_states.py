813ac6e0f437d551342ac98f13c9268a3da5acfc
wahabxvii <auwa.mm@gmail.com>
2026-07-28 20:57:43 +0300

    f

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,7 +1,25 @@
 #!/usr/bin/python3
-from sqlalchemy import create_engine
+'''A scirpt that lists id, name in states table.'''
+import sys
+
 import MySQLdb
 
 
-engine = create_engine("mysql://root:pass@localhost/mydb")
-engine.connect()
+user = sys.argv[1]
+password = sys.argv[2]
+name = sys.argv[3]
+
+
+db = MySQLdb.connect(host="localhost", port=3306, user=user, passwd=password, name=name)
+cursor = db.cursor()
+
+query = "SELECT id, name FROM states ORDER BY id ASC"
+cursor.execute(query)
+
+rows = cursor.fetchall()
+
+for row in rows:
+    print(row)
+
+cursor.close()
+db.close()
