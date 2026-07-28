c43fa580f81ad2143f07476aa7ead79ed8974a23
wahabxvii <auwa.mm@gmail.com>
2026-07-28 21:54:26 +0300

    t

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -5,12 +5,13 @@ import sys
 import MySQLdb
 
 
-user = sys.argv[1]
-password = sys.argv[2]
-name = sys.argv[3]
+db = MySQLdb.connect(
+    host="localhost",
+    port=3306,
+    user=sys.argv[1],
+    passwd=sys.argv[2],
+    name=sys.argv[3])
 
-
-db = MySQLdb.connect(host="localhost", port=3306, user=user, passwd=password, name=name)
 cursor = db.cursor()
 
 query = "SELECT id, name FROM states ORDER BY id ASC"
