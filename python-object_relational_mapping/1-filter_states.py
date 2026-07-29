5afa937c3d815c1d2d427cc0764fefabb143d622
wahabxvii <auwa.mm@gmail.com>
2026-07-29 14:45:28 +0300

    create: file 1

---
+++ python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+'''A scirpt that lists id, name in states table.'''
+import MySQLdb
+
+import sys
+
+
+if __name__ == "__main__":
+
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        name=sys.argv[3])
+
+    cursor = db.cursor()
+
+    query = "SELECT id, name FROM states ORDER BY states.id ASC\
+    WHERE name LIKE 'A%'"
+    cursor.execute(query)
+
+    rows = cursor.fetchall()
+
+    for row in rows:
+        print(row)
+
+    cursor.close()
+    db.close()
