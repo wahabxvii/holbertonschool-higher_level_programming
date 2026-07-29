f8a7abf22aeee5c489411e68fedf3a5a489b6dd0
wahabxvii <auwa.mm@gmail.com>
2026-07-30 01:29:49 +0300

    f

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
-'''A scirpt that lists id, name in states table.'''
-import MySQLdb
+'''A scirpt that lists all states with a name starts with N.'''
 
+import MySQLdb
 import sys
 
 
 if __name__ == "__main__":
 
     db = MySQLdb.connect(
-        host="localhost",
+        host="127.0.0.1",
         port=3306,
         user=sys.argv[1],
         passwd=sys.argv[2],
