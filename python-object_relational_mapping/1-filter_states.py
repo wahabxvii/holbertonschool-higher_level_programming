e8f909ce7c694dfa0b1354d0a76f189c82baa7e0
wahabxvii <auwa.mm@gmail.com>
2026-07-30 02:01:39 +0300

    t

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -1,24 +1,22 @@
 #!/usr/bin/python3
-'''A scirpt that lists all states with a name starts with N.'''
+"""A scirpt that lists all states with a name starts with N."""
 
 import MySQLdb
 import sys
 
 
 if __name__ == "__main__":
-
     db = MySQLdb.connect(
         host="127.0.0.1",
         port=3306,
         user=sys.argv[1],
         passwd=sys.argv[2],
-        database=sys.argv[3])
+        database=sys.argv[3],
+    )
 
     cursor = db.cursor()
 
-    query =(
-        "SELECT * FROM states WHERE name LIKE 'N%' ORDER BY states.id ASC"
-    )
+    query = "SELECT * FROM states WHERE name LIKE 'N%' ORDER BY states.id ASC"
     cursor.execute(query)
 
     rows = cursor.fetchall()
