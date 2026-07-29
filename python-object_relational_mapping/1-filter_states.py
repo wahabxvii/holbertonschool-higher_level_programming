e1338b2fb8e05e630ba2f2d6122ec4b8e8306a10
wahabxvii <auwa.mm@gmail.com>
2026-07-30 01:41:40 +0300

    fix: pycodestyle

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -16,8 +16,8 @@ if __name__ == "__main__":
 
     cursor = db.cursor()
 
-    query = ("SELECT * FROM states WHERE name LIKE 'N%' "
-    "ORDER BY states.id ASC")
+    query = "SELECT * FROM states WHERE name LIKE 'N%' "\
+    "ORDER BY states.id ASC"
     cursor.execute(query)
 
     rows = cursor.fetchall()
