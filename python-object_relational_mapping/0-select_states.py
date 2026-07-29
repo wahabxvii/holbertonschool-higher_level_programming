5d3d035e3acb82d9fa19fd76f69c2f4325219168
wahabxvii <auwa.mm@gmail.com>
2026-07-30 01:26:52 +0300

    t

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,18 +1,18 @@
 #!/usr/bin/python3
 '''A scirpt that lists id, name in states table.'''
-import MySQLdb
 
+import MySQLdb
 import sys
 
 
 if __name__ == "__main__":
 
     db = MySQLdb.connect(
-        host="localhost",
+        host="127.0.0.1",
         port=3306,
         user=sys.argv[1],
         passwd=sys.argv[2],
-        name=sys.argv[3])
+        database=sys.argv[3])
 
     cursor = db.cursor()
 
