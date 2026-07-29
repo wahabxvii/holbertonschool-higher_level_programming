c57d8a7fec004acf92b9b040d4eb65a7ec11161b
wahabxvii <auwa.mm@gmail.com>
2026-07-30 01:35:49 +0300

    fixes

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -12,12 +12,12 @@ if __name__ == "__main__":
         port=3306,
         user=sys.argv[1],
         passwd=sys.argv[2],
-        name=sys.argv[3])
+        database=sys.argv[3])
 
     cursor = db.cursor()
 
-    query = "SELECT * FROM states ORDER BY states.id ASC\
-    WHERE name LIKE 'N%'"
+    query = "SELECT * FROM states WHERE name LIKE 'N%'\
+    ORDER BY states.id ASC"
     cursor.execute(query)
 
     rows = cursor.fetchall()
