8b9674bdff6ad1af842c7ce97fe2785502ddba78
wahabxvii <auwa.mm@gmail.com>
2026-07-30 10:29:37 +0300

    create: flie 5

---
+++ python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""A scirpt that lists id, name in states table."""
+
+import MySQLdb
+import sys
+
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(
+        host="127.0.0.1",
+        port=3306,
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        database=sys.argv[3],
+    )
+
+    cursor = db.cursor()
+
+    state = sys.argv[4]
+    query = ("SELECT cities.name FROM cities\
+    LEFT JOIN states ON states.id = cities.state_id\
+    WHERE BINARY state.name = %s\
+    ORDER BY cities.id ASC", (state,))
+    cursor.execute(query)
+
+    rows = cursor.fetchall()
+
+    for row in rows:
+        print(row)
+
+    cursor.close()
+    db.close()
