ebb825141d511b41e62959114e5ce5ac8a799c5a
wahabxvii <auwa.mm@gmail.com>
2026-07-30 10:12:10 +0300

    create: file 4

---
+++ python-object_relational_mapping/4-citiess_by_state.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""A scirpt that lists id, name in states table."""
+
+import MySQLdb
+import sys
+
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(
+        host="127.0.0.1",
+        port=3306,
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        database=sys.argv[3],
+    )
+
+    cursor = db.cursor()
+
+    query = "SELECT cities.id, cities.name, states.name \
+    from cities LEFT JOIN states ON states.id = cities.state_id\
+    ORDER BY cities.id ASC"
+    cursor.execute(query)
+
+    rows = cursor.fetchall()
+
+    for row in rows:
+        print(row)
+
+    cursor.close()
+    db.close()
