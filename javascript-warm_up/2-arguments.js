3ba802fde19e66efe34f1780db5d76038af59998
wahabxvii <auwa.mm@gmail.com>
2026-08-17 23:33:46 +0300

    fix: update 2-arguments.js logic

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -2,10 +2,10 @@
 
 const args = process.argv.length - 2;
 
-if (args == 0) {
-  console.log("No argument");
-} else if (args == 1) {
-  console.log("Argument found");
+if (args === 0) {
+  console.log('No argument');
+} else if (args === 1) {
+  console.log('Argument found');
 } else {
-  console.log("Arguments found");
+  console.log('Arguments found');
 }
