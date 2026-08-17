a87209c4fc5784c74cdf26a6fa406a8ebae73786
wahabxvii <auwa.mm@gmail.com>
2026-08-18 00:28:00 +0300

    fix

--- javascript-warm_up/3-value_argument.js
+++ javascript-warm_up/3-value_argument.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 
-const arg = process.argv[2]
+const arg = process.argv[2];
 
 if (arg === undefined) {
-  console.log('No argument');
+  console.log("No argument");
 } else {
   console.log(arg);
 }
